8dc95d12ef27c2dbde074777b27dda84a7f9d847
Alicease <73001978+Alicease@users.noreply.github.com>
2020-10-17 05:27:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_project
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
