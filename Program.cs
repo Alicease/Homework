3c6fe7fda22a89b55bfa210b69fee58462a3bbe4
Alicease <73001978+Alicease@users.noreply.github.com>
2020-10-17 05:29:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4_staff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] RandomArr = new int [4,5];
+            Random ran = new Random();
+            for (int i=0; i<4; i++)
+            {
+                for (int j=0; j<5; j++)
+                {
+                    RandomArr[i,j] = ran.Next (1,100);
+                    Console.Write("{0} \t", RandomArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
