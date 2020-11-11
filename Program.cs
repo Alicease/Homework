a9f5d581bf14254197a3ef3e2e74736827156656
Alicease <73001978+Alicease@users.noreply.github.com>
2020-11-11 21:13:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Evk_Nod
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            {
+            int x, y;
+                Console.Write("x= ");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("y= ");
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("НОД = {0}", NOD(x, y));
+                Console.ReadKey();
+
+            }
+            static int NOD(int x, int y)
+            {
+                if (x < 0 || y < 0)
+                {
+                    x = Math.Abs(x);
+                    y = Math.Abs(y);
+                }
+                if (x == 0 && y == 0)
+                {
+                    return 0;
+                }
+                while (x != y)
+                {
+                    if (x > y)
+                        x = x - y;
+                    else
+                        y = y - x;
+                }
+                return x;
+            }
+        }
+    }
